1518f80e896adf66f6a4a3f8fb37c7f7da9f5095
unohayato <mamidori1p1p@gmail.com>
2022-08-15 13:15:07 +0900

    validationerror

--- text_edit/forms.py
+++ text_edit/forms.py
@@ -1,7 +1,15 @@
 
 from django import forms
+from django.core.exceptions import ValidationError
 
 class TextForm(forms.Form):
   text = forms.CharField()
   search = forms.CharField()
   replace = forms.CharField()
+  
+  def clean(self):
+    data = super().clean()
+    text = data['text']
+    if len(text) <= 5:
+      raise ValidationError('テキストが短すぎます。6文字以上入力してください。')
+    return data
--- text_edit/views.py
+++ text_edit/views.py
@@ -1,6 +1,20 @@
+from cgitb import text
+from dataclasses import replace
 from django.views.generic.edit import FormView
 from . import forms
 
 class Index(FormView):
   form_class = forms.TextForm
   template_name = 'text_edit/index.html'
+
+  def form_valid(self, form):
+    data = form.cleaned_data
+    text = data['text']
+    search = data['search']
+    replace = data['replace']
+    
+    new_text = text.replace(search, replace)
+    
+    ctxt = self.get_context_data(new_text=new_text, form = form)
+    return self.render_to_response(ctxt)
+    
